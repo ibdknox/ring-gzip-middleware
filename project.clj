eb9834ad8545756e74eaac6ddd90bed796ac9115
Chris Granger <ibdknox@gmail.com>
2011-12-25 20:29:50 -0500

    move to clojure.java.io and 1.3.0
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject org.clojars.mikejs/ring-gzip-middleware "0.1.0-SNAPSHOT"
+(defproject ibdknox/ring-gzip-middleware "0.1.1"
   :description "Ring gzip encoding middleware"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
